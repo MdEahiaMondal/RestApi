1ae3a267053248a7e3884a7e1c509a29edf523b0
MdEahiaMondal <mdeahiamondal@gmail.com>
2019-11-05 09:57:49 +0600

    product delete successfully done

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -84,14 +84,17 @@ class ProductController extends Controller
         ],Response::HTTP_OK);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
+
+
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+
+        return response()->json([
+            'Deleted' => "Product Deleted successfully",
+        ],Response::HTTP_NOT_FOUND);
+
     }
+
+
 }
