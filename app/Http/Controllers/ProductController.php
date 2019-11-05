e43d547e864f33449e6f8d6d778eaa8fb73527ce
MdEahiaMondal <mdeahiamondal@gmail.com>
2019-11-05 07:31:14 +0600

    now make  ecommerce api route and show all product with normal away

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,14 +7,10 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        //
+        return Product::all();
     }
 
     /**
